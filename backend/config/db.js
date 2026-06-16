b0d5264b67c05f2fcba7c8e74f16ad8291ba8e20
umair uddin <umairuddin491@gmail.com>
2026-06-16 11:23:58 -0700

    Update project files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,33 +1,21 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
-// TEST CONNECTION
+// Test connection
 db.getConnection((err, connection) => {
-
   if (err) {
-
-    console.log(
-      "❌ MySQL Connection Failed:",
-      err.message
-    );
-
+    console.log("❌ MySQL Connection Failed:", err.message);
   } else {
-
     console.log("✅ MySQL Connected");
-
     connection.release();
   }
 });
 
-// IMPORTANT
 module.exports = db;
