94b09a4c3a21f8feeb1e92a8debea92abb8b35d3
umair uddin <umairuddin491@gmail.com>
2026-06-17 06:20:05 -0700

    fix db connection pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "mysql.railway.internal",
-  user: "root",
-  password: "OVOwXrJZYiHhVNnYgwYVwRNcxBNqnEFs",
-  database: "railway",
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 20000,
-  ssl: { rejectUnauthorized: false },
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 });
 
 db.getConnection((err, connection) => {
